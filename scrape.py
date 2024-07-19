d96d1e155b12fefba50ace87aeb7a56a1bd49676
isabellestt <seetisabelle@gmail.com>
2024-07-19 13:31:02 -0400

    Add records to db

--- scrape.py
+++ scrape.py
@@ -7,17 +7,17 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException
 from selenium.common.exceptions import WebDriverException
 
-import pandas as pd
+import json
 import os
 import logging
 from datetime import datetime
 
 class Scraper:
     url = "https://www.singaporepools.com.sg/en/product/Pages/toto_results.aspx"
-    # url = 'https://www.adamchoi.co.uk/overs/detailed'
+    # url = "https://www.singaporepools.com.sg/en/product/sr/Pages/toto_results.aspx?sppl=RHJhd051bWJlcj0zOTg1"
     OUTPUT_DIR = './output'
     logger = logging.getLogger()
     logger.setLevel(logging.INFO)
@@ -26,35 +26,40 @@ class Scraper:
 
     def __init__(self):
         self.start_session()
-        date = datetime.today().strftime('%d%m%Y')
-        self.OUTPUT_DIR = f'{self.OUTPUT_DIR}/{date}'
-        if not os.path.exists(self.OUTPUT_DIR):
-            os.makedirs(self.OUTPUT_DIR)
+
 
     def start_session(self):
         self.driver = webdriver.Chrome()
         self.driver.implicitly_wait(5)
         self.logger.info('Chrome session started')
-
-    def scrape(self):
         self.driver.get(self.url)
         self.logger.info('Scrapping initiated')
-        winning_ticket_details = self.driver.find_element(By.XPATH, "//a[@class='btn btn-orange form-control linkShowWinningOutlets']")
+        winning_ticket_details = self.driver.find_element(By.XPATH,"//a[@class='btn btn-orange form-control linkShowWinningOutlets']")
         winning_ticket_details.click()
 
+    def scrape(self):
         # date
         try:
             wait = WebDriverWait(self.driver, 10)
             date_ele = wait.until(EC.presence_of_element_located((By.XPATH, "//th[@class='drawDate']")))
-            self.record["date"] = date_ele.text
-        except WebDriverException:
+            date_str = date_ele.text.replace(',', '')
+            self.record["date"] = date_str
+            date_str = " ".join(date_str.split(" ")[1:])
+            date_format = "%d %b %Y"
+            date_dir = datetime.strptime(date_str, date_format)
+            date_dir = datetime.strftime(date_dir, '%Y%m%d')
+        except NoSuchElementException:
             self.logger.info('Cannot load date')
+
+        self.OUTPUT_DIR = f'{self.OUTPUT_DIR}/{date_dir}'
+        if not os.path.exists(self.OUTPUT_DIR):
+            os.makedirs(self.OUTPUT_DIR)
 
         # draw number
         try:
             draw_no_text = wait.until(EC.presence_of_element_located((By.XPATH, "//th[@class='drawNumber']")))
             self.record["draw_no"] = draw_no_text.text.split(" ")[2]
-        except WebDriverException:
+        except NoSuchElementException:
             self.logger.info('Cannot load draw number')
 
         # winning numbers
@@ -82,42 +87,51 @@ class Scraper:
         self.record['group_1_outlets'] = []
         self.record['group_2_outlets'] = []
         try:
-            group_1_winning_ticket = self.driver.find_element(By.XPATH, "//strong[text()='Group 1 winning tickets sold at:']")
             ul = self.driver.find_element(By.XPATH, "//div[@class='divWinningOutlets']/ul")
             outlets = ul.find_elements(By.XPATH, "./li")
             for outlet in outlets:
                 self.record['group_1_outlets'].append(outlet.text)
-        except NoSuchElementException:
+        except WebDriverException:
             self.logger.info('No group 1 winner')
 
         try:
-            group_2_winning_ticket = self.driver.find_element(By.XPATH, "//strong[text()='Group 2 winning tickets sold at:']")
             ul = self.driver.find_elements(By.XPATH, "//div[@class='divWinningOutlets']/ul")
-            if len(ul) > 1:
-                ul = ul[1]
-            else:
-                ul = ul[0]
-            outlets = ul.find_elements(By.XPATH, "./li")
-            for outlet in outlets:
-                self.record['group_2_outlets'].append(outlet.text)
-        except NoSuchElementException:
+            if len(ul) > 0:
+                if len(ul) > 1:
+                    ul = ul[1]
+                else:
+                    ul = ul[0]
+                outlets = ul.find_elements(By.XPATH, "./li")
+                for outlet in outlets:
+                    self.record['group_2_outlets'].append(outlet.text)
+        except WebDriverException:
             self.logger.info('No group 2 winner')
 
         self.export_records()
 
     def export_records(self):
-        series = pd.Series(self.record, index=list(self.record.keys()))
-        series.to_csv('records.csv', index=False)
+        # series = pd.Series(self.record, index=list(self.record.keys()))
+        output_path = f'{self.OUTPUT_DIR}/records.json'
+        # series.to_csv(output_path, index=False)
+        with open(output_path, "w") as outfile:
+            json.dump(self.record, outfile)
+
 
     def scrape_past_year(self):
-        select = Select(self.driver.find_element(By.XPATH, "//select[@class='form-control selectDrawList']"))
-        for i in range(365):
-            select.select_by_index(i)
+        for i in range(104):
+            self.OUTPUT_DIR = 'output'
+            try:
+                select = Select(self.driver.find_element(By.XPATH, "//select[@class='form-control selectDrawList']"))
+                select.select_by_index(i)
+                self.scrape()
+            except WebDriverException as e:
+                print('error', WebDriverException)
 
 def run_scraper():
     scraper = Scraper()
-    scraper.scrape()
-    print(scraper.record)
+    scraper.start_session()
+    scraper.scrape_past_year()
+    # print(scraper.record)
 
 run_scraper()
 
